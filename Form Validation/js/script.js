83f0dde05156ac39451818a983f16313bf3d91b3
Khalid Saifullah Fuad <khalidsaifullahfuad@gmail.com>
2021-07-13 17:43:47 +0600

    UI update

--- Form Validation/js/script.js
+++ Form Validation/js/script.js
@@ -1,36 +1,52 @@
 function validateForm(){
     var first_name = document.getElementById('first_name').value;// Using DOM
     var last_name = document.getElementById('last_name').value;// Uning DOM
+    var email = document.getElementById('email');
 
     first_name = capitalize(first_name);
     last_name = capitalize(last_name);
     
     
     if(first_name=="" || last_name=="")// check if first name or last name is empty
-        alert("input field empty");
-    else if(checkSpecialCharacter(first_name) || checkSpecialCharacter(last_name)) // if contains special character
-        alert("contains special character")
-    else
+        setError("Please fill out empty field");
+    else if(checkSpecialCharacter(first_name) || checkSpecialCharacter(last_name) || validateEmail(email)) // if contains special character
+        setError("Contains special character")
+    else{
+        setError("");
         alert(first_name+" "+last_name);   
-
+    }
     return false; // for stop form to reload
 }
 
 function checkSpecialCharacter(str){
-    var special_chars = "- $&#.,;:!\"\'<>_"; // list of special character 
+    var special_chars = " `!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?~"; // list of special character 
     var hasSpecialChar = false;
 
     for(let i=0; i<special_chars.length; i++){ // traverse on special_chars 
         // checking if str contains any of special_chars character
-        if(str.search(special_chars[i].toString())!=-1){ // search retuns positive index if the character is found or returns -1 if the charcter don't appear in str
+        if(str.includes(special_chars[i])){ // search retuns positive index if the character is found or returns -1 if the charcter don't appear in str
             hasSpecialChar = true;
+            break;
         }
     }
     
     return hasSpecialChar;
 }
 
+
+function setError(errorMsg){
+    // document.getElementById('first_name').style.borderColor = "red";
+    document.getElementById("error-msg").innerText = errorMsg;
+}
+
 function capitalize(str){
     str = str.trim(); // remove space from front and back
     return str.substr(0,1).toUpperCase() + str.substr(1).toLowerCase(); // uppercase first letter and lowercase rest of letter
+}
+
+
+function validateEmail(email){
+    var email_str = email.value;
+
+    if
 }
