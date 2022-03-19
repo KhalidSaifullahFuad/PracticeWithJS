481a62d7e9d551e1ab15970ad624fa22de5c9525
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-03-19 20:39:23 +0600

    🐛  Fixed some typos and removed dead code

--- Cube Timer/js/script.js
+++ Cube Timer/js/script.js
@@ -1,22 +1,19 @@
-var hour = document.getElementById("hour"); 	// * selcted hour using dom;
-var min = document.getElementById("min"); 		// * selcted min using dom;
-var sec = document.getElementById("sec"); 		// * selcted sec using dom;
-var milis = document.getElementById("milis"); 	// * selcted sec using dom;
+const hour = document.getElementById("hour"); 	// * selected hour using dom;
+const min = document.getElementById("min"); 	// * selected min using dom;
+const sec = document.getElementById("sec"); 	// * selected sec using dom;
+const milis = document.getElementById("milis"); // * selected sec using dom;
 
-var isTimerOn = false; 							// * for detecting if the timer is on/off
-var allowed = true;								// * for detecting the space key repeating
+let isTimerOn = false; 							// * for detecting if the timer is on/off
+let allowed = true;								// * for detecting the space key repeating
 
-var countdown; 									// * for assigning the setinterval and clearinterval
+var countdown; 									// * for assigning the setInterval and clearinterval
 
 window.addEventListener("keydown", function (e) {
-	if (e.repeat != undefined) {
-		allowed = !e.repeat;
-	}
+	if (e.repeat != undefined) allowed = !e.repeat;
 	if (!allowed) return;
 	allowed = false;
 
-	if (e.keyCode == 32) {
-		console.log("keydown - " + isTimerOn);
+	if (e.code == "Space") {
 		if (!isTimerOn) {
 			hour.innerText = "00";
 			min.innerText = "00";
@@ -34,40 +31,25 @@ window.addEventListener("keydown", function (e) {
 });
 
 window.addEventListener("keyup", function (e) {
-	if (e.keyCode == 32 && isTimerOn) {
+	if (e.code == "Space" && isTimerOn) {
+		console.log(e);
 		countdown = setInterval(function(){
-			milis.innerText = formatTime(milis.innerText);
-			checkTime(milis, sec, 100);
-			checkTime(sec, min, 60);
-			checkTime(min, hour, 60);
+			milis.innerText = addPadding(milis.innerText);
+			formatTime(milis, sec, 100);
+			formatTime(sec, min, 60);
+			formatTime(min, hour, 60);
 		}, 10);
 	}
 });
 
 
-function checkTime(time1, time2, limit) {
+function formatTime(time1, time2, limit) {
 	if (time1.innerText == limit) {
 		time1.innerText = "".padStart(`${limit - 1}`.length, 0);
-		time2.innerText = formatTime(time2.innerText);
+		time2.innerText = addPadding(time2.innerText);
 	}
 }
 
-function formatTime(time) {
-	return (parseInt(time) + 1).toString().padStart(time.length, "0"); // * added 1 to prevous value after pasring to int, added zero in start
+function addPadding(time) {
+	return (parseInt(time) + 1).toString().padStart(time.length, "0"); // * added 1 to previous value after parsing to int, added zero in start
 }
-
-//if (sec.innerText == 60) {
-//     sec.innerText = "00";
-//     min.innerText = (parseInt(min.innerText) + 1).toString().padStart(2, "0");
-
-//     if (min.innerText == 60) {
-//         min.innerText = "00";
-//         hour.innerText = (parseInt(hour.innerText) + 1).toString().padStart(2, "0");
-
-//         if (hour.innerText == 12) {
-//             hour.innerText = "00";
-//             hour.innerText.padStart(2, "0");
-//             clearInterval(countdown);
-//         }
-//     }
-// }
